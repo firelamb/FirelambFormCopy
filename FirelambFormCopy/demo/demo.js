3fd2d94291a7cd5437b99eb44a75c18b3af22c08
firelamb <firelamb@qq.com>
2014-07-23 19:22:42 +0800

    add a new option 'callevent'

---
+++ FirelambFormCopy/demo/demo.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+
+  
+    //$("#demoButton").on("click", function () {
+    $("#demoButton").FirelambFormCopy('copy', {
+        formTo: "#demoButton"
+        ,formId:"#coyform"        
+        });
+    //});
+
+    $("#demoButton1").FirelambFormCopy('copy', {
+        formTo: "#divTo1"
+          , formId: "#coyform"
+        , triggerEventType: "click"
+            ,copyType:"append"
+    });
+
+    $("#demoButton2").FirelambFormCopy('copy', {
+        formTo: "#demoButton2"
+      , formId: "#coyform"
+           , copyType: "before"
+    });
+
+    $("#demoButton4").on("click", function () {
+        $("#demoButton4").FirelambFormCopy('recover');
+    });
+   
+
+});
